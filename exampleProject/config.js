47dc701f2d796ffcf60547722c6e314e0fa89a10
Steffen Tiedemann Christensen <steffentchr@steffentchr.local>
2010-02-02 22:35:56 +0100

    Complete session API for bomber utilizing on-disk persistent storage, secure cookies and configurable expiration.
    The SessionStore object is created during server startup to manage the session and store them in memory. Each session is matched to a secure cookie and assigned upon request to clients.
    
    A session object (the same object, in fact) is available in both request and response. Options are:
    (request|response).session.set(name, value)
    (request|response).session.get(name)
    (request|response).session.unset(name)
    (request|response).session.reset()
    (request|response).session.exists(name)
    (request|response).session.keys()
    (request|response).session.save()
    (request|response).session.possiblyRenewSessionCookie()
    (request|response).session.writeSessionCookie()

--- exampleProject/config.js
+++ exampleProject/config.js
@@ -1,4 +1,5 @@
-/* PROJECT CONFIGURATION FILE */
+
+// *** BASIC CONFIGURATION *** 
 
 // Port to run node server on (default 8400)
 // exports.port = 8400;
@@ -6,6 +7,9 @@
 // A secret key to sign sessions, cookies, password etc. 
 // Make sure you set this, and that you keep the secret -- well, secret
 exports.signing_secret = "";
+
+
+// *** PERSISTENT STORAGE *** 
 
 // Configuration for persistent storage for session and users
 // (at the moment, only on-disk storage is available)
@@ -16,11 +20,30 @@ exports.persistent_storage_method = 'disk';
 // Default: '/tmp/'
 exports.persistent_storage_location = './storage/';
 
+
+// *** SESSIONS *** 
+
 // Minutes before sessions expire
-// Default: 120
-exports.session_expire_minutes = 120;
+// Default: 60
+exports.session_expire_minutes = 3;
+
+// Minutes before a session is renewed
+// Default: 5
+exports.session_renew_minutes = 1;
 
 // Name of the cookie to use for session
 // Default: 'session_key'
 // exports.session_cookie_name = 'session_key';
+
+// Domain for the session cookie
+// Default: ''
+// exports.session_cookie_domain = '';
+
+// Path for the session cookie
+// Default: '/'
+// exports.session_cookie_path = '/';
+
+// Whether to require session cookies only to be sent over secure connections (Boolean)
+// Default: false
+// exports.session_cookie_secure = false;
 
